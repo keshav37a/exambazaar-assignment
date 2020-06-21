39c7deda6f367786495d39b52d8258c71bb56747
Keshav Mathur <keshav37a@gmail.com>
2020-06-21 18:44:37 +0530

    add images conditional rendering; add numericalType conditional rendering

--- client/src/components/RandomQuestion.js
+++ client/src/components/RandomQuestion.js
@@ -32,22 +32,39 @@ const RandomQuestion = (props) => {
     }
   }
 
+  let ImageOutsideQuestionsArr = (props)=>{
+    if(props.images===null){
+        return(<span></span>);
+    }
+    let images = props.images.map((image, index)=>{
+        return(
+            <img key={index} alt="question-image" src={image}></img>
+        )
+    })
+    return images;
+  }
+
+
   let RenderInputTypeOptions = (props) => {
     let mcqma = props.mcqma;
     let options = props.options;
     let qIndex = props.qIndex;
     let opts = [];
+    let type = props.type;
     console.log(mcqma);
-    if (!mcqma) {
+    if(type==="numerical"){
+        return (
+            <div className="singleOptionContainer">
+              <input className="inputText" type="text" placeholder="Write Your Answer Here...." name={qIndex}></input>
+              <button className="inputSubmit" type="submit">Mark</button>
+            </div>
+          );
+    }
+    else if (!mcqma) {
       opts = options.map((option, oIndex) => {
         return (
           <div className="singleOptionContainer" key={oIndex}>
-            <input
-              className="inputRadio"
-              type="radio"
-              name={qIndex}
-              value={option.option}
-            ></input>
+            <input className="inputRadio" type="radio" name={qIndex} value={option.option}></input>
             <span className="singleOptionText">{option.option}</span>
           </div>
         );
@@ -58,12 +75,7 @@ const RandomQuestion = (props) => {
       opts = options.map((option, oIndex) => {
         return (
           <div className="singleOptionContainer" key={oIndex}>
-            <input
-              className="inputRadio"
-              type="checkbox"
-              name={qIndex}
-              value={option.option}
-            ></input>
+            <input className="inputRadio" type="checkbox" name={qIndex} value={option.option}></input>
             <span className="singleOptionText">{option.option}</span>
           </div>
         );
@@ -75,7 +87,7 @@ const RandomQuestion = (props) => {
 
   console.log(randomQuestion);
 
-  if (randomQuestion.question != undefined) {
+  if (randomQuestion.question !== undefined) {
     return (
       <div id="randomQuestionContainer">
         <span className="examNameText">
@@ -92,15 +104,24 @@ const RandomQuestion = (props) => {
         <div className="context justText">
           {randomQuestion.question.context}
         </div>
+        <ImageOutsideQuestionsArr images={randomQuestion.question.images}/>
+
         {randomQuestion.question.questions.map((question, qIndex) => {
           return (
             <div key={qIndex} className="singleQuestionContainer">
+            <ImageOutsideQuestionsArr images={question.images}/>
               <div className="questionText">{question.question}</div>
+              {question.images.map((image)=>{
+                  return(
+                      <img></img>
+                  )
+              })}
               <div>
                 <RenderInputTypeOptions
                   mcqma={question.mcqma}
                   options={question.options}
                   qIndex={qIndex}
+                  type={question.type}
                 />
               </div>
             </div>
