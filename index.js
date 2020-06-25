57a9bbb3e4422336521b8cdbdfe47fc5c0e52fa6
Keshav Mathur <keshav37a@gmail.com>
2020-06-25 18:13:04 +0530

    pushing to heroku

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const path = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", require("./routes/index"));
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, (err) => {
   if (err) {
